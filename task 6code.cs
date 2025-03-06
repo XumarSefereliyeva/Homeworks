878c1ade2fcd9820659848ee68a7a3db76508204
XumarSefereliyeva <xumarsefereliyeva@gmail.com>
2025-03-06 12:00:29 +0300

    Task 6 code

---
+++ task 6code.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_6_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("a daxil edin");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b daxil edin");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = 5;
+            b = 10;
+            (a, b) = (b, a);
+
+            Console.WriteLine(a);
+        }
+    }
+}
