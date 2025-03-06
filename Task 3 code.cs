7eacd0cf0ad81a51874ef025becc160c7267de45
XumarSefereliyeva <xumarsefereliyeva@gmail.com>
2025-03-06 10:43:37 +0300

    Task 3 code

---
+++ Task 3 code.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+namespace Task_3_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maasinizi daxil edin");
+            int maas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kreditinizi daxil edin");
+            int kredit = Convert.ToInt32(Console.ReadLine());
+            int illikmaas = maas * 12;
+            if (kredit < illikmaas * 0.6)
+            {
+                Console.WriteLine("kredit goture bilersiniz");
+
+            }
+            else
+            {
+                Console.WriteLine("Kredit goture bilmezsiniz");
+            }
+        }
+    }
+}
+
