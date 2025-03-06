2162377140179f0e2c5d6413496dcfb78d41212a
XumarSefereliyeva <xumarsefereliyeva@gmail.com>
2025-03-06 11:32:18 +0300

    Task5 code.css

---
+++ Task 5 code.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_5_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eni daxil edin");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzunlugu daxil edin");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int P = 2 * (a + b);
+            int S = (a * b);
+            Console.WriteLine("Sahe:" + S);
+            Console.WriteLine("Perimetr:" + P);
+
+
+
+        }
+    }
+}
+    
