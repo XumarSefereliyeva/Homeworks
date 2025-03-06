e51546f67509b60dabaab979d528d17df53eb19b
XumarSefereliyeva <xumarsefereliyeva@gmail.com>
2025-03-06 11:04:36 +0300

    Task 4 code

---
+++ Task 4code.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_4_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int onluq = num / 10;
+            int teklik = num % 10;
+            if (onluq > teklik)
+            {
+                Console.WriteLine("Onluq boyukdur");
+            }
+            else
+            {
+                Console.WriteLine("Teklik boyukdur");
+            }
+        }
+    }
+}
